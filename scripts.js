1fe4fbb5c950b3dd28bc3bf3e16db404082a5d00
Emanuele <feliziani.emanuele@gmail.com>
2015-08-09 23:43:28 +0200

    Published!
    The website is live. I've written a lightbox from scratch and improved the maintainability of a lot of things. I added text and all. Things are looking pretty solid. Cheers!
    
    Signed-off-by: Emanuele <feliziani.emanuele@gmail.com>

--- scripts.js
+++ scripts.js
@@ -3,42 +3,73 @@ document.addEventListener('DOMContentLoaded', function() {
 		viewport = document.documentElement.clientWidth,
 		deviceHeight = document.documentElement.clientHeight - 50,
 		hash = window.location.hash.substring(1),
-		contattiInner = document.getElementById('contatti-inner'),
-		designersSecondary = document.querySelector('#designers .secondary'),
+		menuHelper = document.getElementById('menu-helper'),
+		closeMenu = document.getElementById('close-menu'),
+		contattiInner = document.getElementById('contatti'),
+		aboutSection = document.getElementById('about'),
+		designersSection = document.getElementById('designers'),
+		beenDoneA = false,
 		beenDoneC = false,
 		beenDoneP = false,
 		copyright = document.getElementById('copyright'),
-		telLink = document.getElementsByClassName('telLink'),
 		mapLink = document.getElementsByClassName('mapLink'),
-		isiOSSafari = (navigator.userAgent.match(/Mac OS X/i)) ? true: false;
+		isApple = (navigator.userAgent.match(/Mac OS X/i)) ? true: false,
+		toBlur = document.querySelector( '.to-blur' );
+	
+	function enlarge( imageToEnlarge ) {
+		console.log('enlarge');
+		// Create loading thing
+		var waitEl = document.createElement( 'div' );
+		waitEl.classList.add( 'loading' );
+		// Create the container
+		var enlargingContainer = document.createElement( 'div' );
+		enlargingContainer.classList.add( 'enlarging-container' );
+		// Create the image
+		var image = new Image();
+		image.src = imageToEnlarge.getAttribute( 'src' ).replace( '/thumbs', '' );
+		image.className = 'enlarged';
+		
+		toBlur.classList.add( 'blurred' );
+		
+		enlargingContainer = document.body.appendChild( enlargingContainer );
+		waitEl = enlargingContainer.appendChild( waitEl );
+		function makeItShow() {
+			enlargingContainer.classList.add( 'visible' );
+		}
+		setTimeout( makeItShow, 1 );
+		
+		image = enlargingContainer.appendChild( image );
+		image.onload = function () {
+			enlargingContainer.removeChild( waitEl );
+			image.classList.add( 'loaded' );
+		};
+		// Let's prepare to delete the thing
+		function deleteMe() {
+			document.body.removeChild( enlargingContainer );
+		}
+		enlargingContainer.addEventListener('click', function() {
+			image.classList.remove( 'loaded' );
+			enlargingContainer.classList.remove( 'visible' );
+			toBlur.classList.remove( 'blurred' );
+			setTimeout( deleteMe, 300 );
+		}, false);
+	}
 
 	function enlargeImgs() {
-		[].forEach.call(document.querySelectorAll('.secondary__img'), function(el) {
-			el.addEventListener('click', function() {
-				var popped = document.querySelector('.popped'),
-					classes = this.classList;
-				if ( popped ) {
-					if ( classes.contains('popped') ) {
-						classes.remove('popped');
-					} else {
-						popped.classList.remove('popped');
-						classes.add('popped');
-					}
-				} else {
-					classes.add('popped');
-				}
-			});
+		[].forEach.call(document.querySelectorAll( '.to-enlarge' ), function(el) {
+			el.addEventListener('click', function(event) {
+				event.stopImmediatePropagation();
+				console.log('clicked');
+				enlarge( el );
+			}, true);
 		});
 	}
 
-	function lazyLoad( thing ) {
-		thing.innerHTML = thing.innerHTML.replace(/<!--((.|\n|\r)*?)-->/g, '$1');
-		if ( thing === contattiInner ) {
-			beenDoneC = true;
-		} else if ( thing === designersSecondary ) {
+	function lazyLoad( where ) {
+		[].forEach.call( where.querySelectorAll( '.lazy-me' ), function ( el ) {
+			el.innerHTML = el.innerHTML.replace(/<!--((.|\n|\r)*?)-->/g, '$1');
 			enlargeImgs();
-			beenDoneP = true;
-		}
+		} );
 	}
 
 	if ( !hash ) {
@@ -46,46 +77,59 @@ document.addEventListener('DOMContentLoaded', function() {
 	} else if ( hash === 'contatti') {
 		lazyLoad( contattiInner );
 	} else if ( hash === 'designers' ) {
-		lazyLoad( designersSecondary );
+		lazyLoad( designersSection );
+	} else if ( hash === 'about' ) {
+		lazyLoad( aboutSection );
 	}
 
-	if ( viewport < 767 ) {
-		document.getElementById('menu-helper').addEventListener('click', function() {
-			menuContainer.classList.add('menu-open');
-		});
-
-		document.getElementById('close-menu').addEventListener('click', function() {
-			menuContainer.classList.remove('menu-open');
-		});
-
-		[].forEach.call( document.querySelectorAll('#menu li'), function(el) {
-			el.addEventListener('click', function() {
+	function viewportDependantScripts() {
+		if ( viewport < 767 ) {
+			menuHelper.addEventListener('click', function() {
+				menuContainer.classList.add('menu-open');
+			});
+	
+			closeMenu.addEventListener('click', function() {
 				menuContainer.classList.remove('menu-open');
-			}, false);
-		});
-
-		[].forEach.call( document.querySelectorAll('section'), function(el) {
-			el.style.height = deviceHeight + 'px';
-
-			// Fix iOS7 Safari bug preventing scrolling with -webkit-overflow-scrolling: touch;
-			el.addEventListener('touchstart', function(event){});
-		});
-
-	//	document.querySelectorAll('section').style.height = deviceHeight + 'px';
+			});
+	
+			[].forEach.call( document.querySelectorAll('#menu li'), function(el) {
+				el.addEventListener('click', function() {
+					menuContainer.classList.remove('menu-open');
+				}, false);
+			});
+			
+			// Let's make sure that we always have the correct height, so that content is always perfectly visible on screen
+			toBlur.style.height = deviceHeight + 50 + 'px';
+			[].forEach.call( document.querySelectorAll('section'), function(el) {
+				el.style.height = deviceHeight + 'px';
+	
+				// Fix iOS7 Safari bug preventing scrolling with -webkit-overflow-scrolling: touch;
+				el.addEventListener('touchstart', function(){});
+			});
+	
+		//	document.querySelectorAll('section').style.height = deviceHeight + 'px';
+		}
+	
+		if ( viewport < 1025 ) {
+			// (most)Tablet scripts
+		}
+	
+		if ( viewport > 1200 ) {
+			// Desktop scripts
+		}
 	}
-
-	if ( viewport < 1025 ) {
-		// (most)Tablet scripts
-		FastClick.attach(document.body);
-	}
-
-	if ( viewport > 1200 ) {
-		// Desktop scripts
-	}
+	
+	viewportDependantScripts();
+	
+	document.getElementById('about-link').addEventListener('click', function() {
+		if ( beenDoneA === false ) {
+			lazyLoad( aboutSection );
+		}
+	}, false);
 
 	document.getElementById('designers-link').addEventListener('click', function() {
 		if ( beenDoneP === false ) {
-			lazyLoad( designersSecondary );
+			lazyLoad( designersSection );
 		}
 	}, false);
 
@@ -95,28 +139,19 @@ document.addEventListener('DOMContentLoaded', function() {
 		}
 	}, false);
 
-//	[].forEach.call( document.querySelectorAll('#menu li'), function(el) {
-//		el.addEventListener('click', function() {
-//			if ( beenDone === false ) {
-//				contattiInner.innerHTML = contattiInner.innerHTML.replace(/<!--((.|\n|\r)*?)-->/g, '$1');
-//				beenDone = true;
-//			}
-//		}, false);
-//	});
-
-	if ( !isiOSSafari ) {
+	if ( !isApple ) {
 		[].forEach.call( mapLink, function(el) {
 			var coords = el.getAttribute('data-coords');
 			el.setAttribute('href', 'https://maps.google.com/maps?q='+ coords +'&t=m&z=17');
 		});
-	};
+	}
 
 	// Recalculate viewport size after resize (smarty!)
 	var resizeFired = false,
 		drawing = false,
-		requestAnimationFrame = window.requestAnimationFrame ||
-								window.mozRequestAnimationFrame ||
-								window.webkitRequestAnimationFrame;
+		requestAnimFrame = window.requestAnimationFrame ||
+							window.mozRequestAnimationFrame ||
+							window.webkitRequestAnimationFrame;
 
 	function throttleResize() {
 		// set resizeFired to true and execute drawResize if it's not already running
@@ -133,12 +168,13 @@ document.addEventListener('DOMContentLoaded', function() {
 			drawing = true;
 
 			document.getElementById('container').style.zIndex = 1;
+			viewportDependantScripts();
 
-			requestAnimationFrame(drawResize);
+			requestAnimFrame(drawResize);
 		} else {
 			drawing = false;
 		}
 	}
 	window.addEventListener('resize', throttleResize, false);
-
+	FastClick.attach(document.body);
 });
